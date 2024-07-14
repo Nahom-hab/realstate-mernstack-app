f9822b1a9cf7e21cb457e59a47b9b90c9584358c
Nahom-hab <nahomhabtamu147@gmail.com>
2024-07-14 04:17:08 -0400

    added middleware in index.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs'
 
-export const signup=async (req,res)=>{
+export const signup=async (req,res,next)=>{
    const {username,password,email}=req.body
    const hashedpassword=bcryptjs.hashSync(password,10)
-   const newuser=new User({username,passsword :hashedpassword,email})
+   const newuser=new User({username,password :hashedpassword,email})
    try{
     await newuser.save()
     res.status(200).json('user created succsusfully')  
    }catch(err){
-    res.status(500).json(err.message)
+    next(err)
    }
    
 }
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './styles.module.css'
 
+
 export default function Home() {
   return (
     <div>H</div>
