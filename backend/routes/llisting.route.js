bdd0417ebb8c2a92362af84d240684085b012a04
Nahom-hab <nahomhabtamu147@gmail.com>
2024-07-25 14:09:34 -0400

    finsished searching api fro listing

--- backend/routes/llisting.route.js
+++ backend/routes/llisting.route.js
@@ -1,7 +1,7 @@
 
 import express from 'express'
 import { Router } from 'express'
-import { createListing, deleteListing, editListing, getlisting } from '../controller/listing.controller.js'
+import { createListing, deleteListing, editListing, getlisting,searchListings } from '../controller/listing.controller.js'
 import { verifyUser } from '../utils/verifyuser.js'
 
 const router=Router()
@@ -10,6 +10,7 @@ router.post('/create',verifyUser,createListing)
 router.post('/editListing/:id',verifyUser,editListing)
 router.delete('/deleteListing/:id',verifyUser,deleteListing)
 router.get('/getListing/:id',getlisting)
+router.get('/get',searchListings)
 
 
 export default router
